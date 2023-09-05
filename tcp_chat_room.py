d76cf537b999763948d457e5ef8e7a5d63035c60
Rishi Prasad Narayan Singh <122547010+Rishiprogramer@users.noreply.github.com>
2023-09-05 22:13:46 +0530

    Update tcp_chat_room.py

--- tcp_chat_room.py
+++ tcp_chat_room.py
@@ -1,36 +1,42 @@
 import threading
 import socket
+from datetime import time
 
-host = "YOUR LOCAL SERVER IP ADDRESS"
+host = socket.gethostname()
+ip = socket.gethostbyname(host)
 port = 5412
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((host, port))
+server.bind((ip, port))
 server.listen()
-
+print(f"Started listening at ip address {ip} and port {port}")
 clients = []
 nicknames = []
 
 
-def broadcast(message):
+def broadcast(message, sender_client):
     for client in clients:
-        client.send(message)
+        try:
+            client.send(message.encode('UTF-16'))
+        except Exception as e:
+            print(f"Error sending message to {client}: {e}")
 
 
 def handle(client):
     while True:
         try:
-            message = client.recv(1024)
-            broadcast(message)
+            message = client.recv(4096).decode("UTF-16")
+            if not message:
+                break
+            broadcast(message, client)
 
         except ConnectionError:
-
             index = clients.index(client)
             clients.remove(client)
             client.close()
             nickname = nicknames[index]
             nicknames.remove(nickname)
-            broadcast(f"{nickname} has been disconnected".encode('ascii'))
+            broadcast(f"{nickname} has been disconnected", client)
             break
 
 
@@ -38,19 +44,22 @@ def receive_clients():
     while True:
         client, address = server.accept()
         print("Connected with", address)
+        try:
+            client.send("nickname".encode('UTF-16'))
+            nickname = client.recv(1024).decode('UTF-16')
 
-        client.send("nickname".encode('ascii'))
-        nickname = client.recv(1024).decode('ascii')
+            nicknames.append(nickname)
+            clients.append(client)
+            print("Nickname of the client is", nickname)
+            broadcast(f"{nickname} joined the chat \n", client)
+            client.send("Connected to the server \n".encode('UTF-16'))
 
-        nicknames.append(nickname)
-        clients.append(client)
-        print("Nickname of the client is", nickname)
-        broadcast(f"{nickname} joined the chat".encode('ascii'))
-
-        client.send("Connected to the server".encode('ascii'))
-
-        thread = threading.Thread(target=handle, args=(client,))
-        thread.start()
+            thread = threading.Thread(target=handle, args=(client,))
+            thread.start()
+        except Exception as e:
+            client.close()
 
 
-receive_clients()
+receive_thread = threading.Thread(target=receive_clients)
+receive_thread.start()
+
